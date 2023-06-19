92a11fd2188c965f6688831869889cb2611a91f8
soaresWT <67922757+soaresWT@users.noreply.github.com>
2023-06-19 14:18:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-var cors = require("cors");
-
 const sigaa = require("./modules/sigaa");
 const creditos = require("./modules/creditos");
 const cardapio = require("./modules/cardapio");
-
+const cors = require("cors");
 const app = express();
-
 app.use(cors());
 
 
