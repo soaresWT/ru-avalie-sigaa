d12747e30e2893f35259f7691b7d0e996446a365
soaresWT <67922757+soaresWT@users.noreply.github.com>
2023-06-24 11:15:57 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,29 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+
 const sigaa = require("./modules/sigaa");
 const creditos = require("./modules/creditos");
 const cardapio = require("./modules/cardapio");
-// const app = express();
-// var cors = require('cors');
 
-
-
+const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// app.use(function (req, res, next) {
-//     res.header('Access-Control-Allow-Credentials', true);
-//     res.header('Access-Control-Allow-Origin', req.headers.origin);
-//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//     res.header('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization');
-//     app.use(cors({origin:true,credentials: true}));
-//     if (req.method === "OPTIONS") {
-//         return res.status(200).end();
-//     } else {
-//         next();
-//     }
-    
-// });
 
 const config = {
   port: process.env.PORT || 3000,
@@ -35,7 +19,6 @@ app.get("/", (_, res) => {
 
 app.post("/sigaa", (req, res) => {
   const { login, senha } = req.body;
-  console.log(login, senha);
   sigaa.access(login, senha).then((response) => {
     res.send(sigaa.scrape(response, login));
   });
