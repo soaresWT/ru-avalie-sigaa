bd33fd4e18772256f0450af2bc359372f508f80d
soaresWT <67922757+soaresWT@users.noreply.github.com>
2023-06-24 13:15:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const creditos = require("./modules/creditos");
 const cardapio = require("./modules/cardapio");
 
 const app = express();
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+})
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
