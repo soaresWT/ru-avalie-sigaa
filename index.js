f208aa1ceb0e5b4b52d538a7338cbd68fdd8a7cf
soaresWT <67922757+soaresWT@users.noreply.github.com>
2023-06-24 10:28:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+})
+
 const config = {
   port: process.env.PORT || 3000,
 };
