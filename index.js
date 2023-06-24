9de29f5d9a59b8d90e53b188eaff2c8996bdd3c8
soaresWT <67922757+soaresWT@users.noreply.github.com>
2023-06-24 11:11:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,27 +3,27 @@ const bodyParser = require("body-parser");
 const sigaa = require("./modules/sigaa");
 const creditos = require("./modules/creditos");
 const cardapio = require("./modules/cardapio");
-const app = express();
-var cors = require('cors');
+// const app = express();
+// var cors = require('cors');
 
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization');
-    app.use(cors({origin:true,credentials: true}));
-    if (req.method === "OPTIONS") {
-        return res.status(200).end();
-    } else {
-        next();
-    }
+// app.use(function (req, res, next) {
+//     res.header('Access-Control-Allow-Credentials', true);
+//     res.header('Access-Control-Allow-Origin', req.headers.origin);
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//     res.header('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization');
+//     app.use(cors({origin:true,credentials: true}));
+//     if (req.method === "OPTIONS") {
+//         return res.status(200).end();
+//     } else {
+//         next();
+//     }
     
-});
+// });
 
 const config = {
   port: process.env.PORT || 3000,
